0dfe6e2f1504157c7cf9d014f3e72f76e1be3a6a
valerii.korniienko <vakor.dev@gmail.com>
2023-09-06 20:24:37 +0300

    Task 5

--- Laba2/Components1/src/app/chil.component.ts
+++ Laba2/Components1/src/app/chil.component.ts
@@ -1,12 +1,23 @@
-import {Component} from '@angular/core';
+// import {Component} from '@angular/core';
+// @Component({
+//   selector: 'child-comp',
+//   template: `<h2>Ласкаво просимо {{name}}!</h2>`,
+//   styles: [`h2, p {
+//     color: red;
+//   }`]
+// })
+// export class ChildComponent {
+//   name = "Тарас";
+// }
 
+import { Component } from '@angular/core';
 @Component({
   selector: 'child-comp',
-  template: `<h2>Ласкаво просимо {{name}}!</h2>`,
-  styles: [`h2, p {
-    color: red;
-  }`]
+  template: `<ng-content></ng-content>
+<p>Привіт {{name}}</p>`,
+  styles: [`h2, p {color:red;}`]
 })
+
 export class ChildComponent {
-  name = "Тарас";
+  name= "Тарас";
 }
