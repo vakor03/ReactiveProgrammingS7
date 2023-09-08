9dc63dc628b6d47876cbce136e61729a70dacf66
valerii.korniienko <vakor.dev@gmail.com>
2023-09-08 14:53:56 +0300

    Built-In Pipes

--- Course/angular-course/src/app/app.component.ts
+++ Course/angular-course/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  rate = 0.67;
+
   onCourseSelected(course: Course) {
     console.log("App component - Course selected: ", course)
   }
