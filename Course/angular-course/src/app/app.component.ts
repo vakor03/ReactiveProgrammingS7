1968aeadadb5eebb6f7a0c974a50d34dfc3fe2de
valerii.korniienko <vakor.dev@gmail.com>
2023-09-08 15:09:09 +0300

    @ViewChild('name')

--- Course/angular-course/src/app/app.component.ts
+++ Course/angular-course/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from "./model/course";
 import {CourseCardComponent} from "./course-card/course-card.component";
@@ -12,10 +12,21 @@ export class AppComponent {
 
   courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
+  @ViewChild('cardRef1')
+  card1: CourseCardComponent;
+
+  @ViewChild('cardRef2', {read: ElementRef})
+  card2: ElementRef;
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
 
   onCourseSelected(course: Course) {
-    console.log("App component - Course selected: ", this.card)
+
+    console.log("card 1", this.card1);
+
+    console.log("card 2", this.card2);
+
+    console.log("container", this.containerDiv);
   }
 }
