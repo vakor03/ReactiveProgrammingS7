0d4f22c95987edcf937b193883804261aa38cb1c
valerii.korniienko <vakor.dev@gmail.com>
2023-09-08 15:02:36 +0300

    @ViewChild(Component)

--- Course/angular-course/src/app/app.component.ts
+++ Course/angular-course/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from "./model/course";
+import {CourseCardComponent} from "./course-card/course-card.component";
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,10 @@ export class AppComponent {
 
   courses = COURSES;
 
-  startDate = new Date(2000, 0, 1);
-
-  title = COURSES[0].description;
-
-  price = 9.99;
-
-  rate = 0.67;
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
 
   onCourseSelected(course: Course) {
-    console.log("App component - Course selected: ", course)
+    console.log("App component - Course selected: ", this.card)
   }
 }
