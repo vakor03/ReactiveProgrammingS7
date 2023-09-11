c794dd9c80e1eabea3bd9f88f3393e4078a11c00
valerii.korniienko <vakor.dev@gmail.com>
2023-09-11 16:19:52 +0300

    View Encapsulation finished

--- Course/angular-course/src/app/app.module.ts
+++ Course/angular-course/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { HighlightedDirective } from './directives/highlighted.directive';
+import { NgxUnlessDirective } from './directives/ngx-unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseCardComponent,
-    HighlightedDirective
+    HighlightedDirective,
+    NgxUnlessDirective
   ],
   imports: [
     BrowserModule,
