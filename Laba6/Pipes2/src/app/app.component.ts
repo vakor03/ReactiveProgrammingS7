df9165375662c331226276cc2f1ef37cedc89514
valerii.korniienko <vakor.dev@gmail.com>
2023-09-29 13:05:46 +0300

    Pipes2 finished

--- Laba6/Pipes2/src/app/app.component.ts
+++ Laba6/Pipes2/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import {interval, map, Observable} from "rxjs";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <input [(ngModel)]="num" name="fact">
+    <div>Результат: {{num | format}}</div>
+    <hr/>
+    <input #user name="user" class="form-control">
+    <button class="btn" (click)="users.push(user.value)">Add</button>
+    <p>{{users | join}}</p>
+    <p>Модель: {{phone | async}}</p>
+  `
 })
 export class AppComponent {
-  title = 'Pipes2';
+  num: number = 15.45;
+  users = ["Tom", "Alice", "Sam", "Kate", "Bob"];
+  phones = ["iPhone 7", "LG G 5", "Honor 8", "Samsung Galaxy S7", "Motorola Moto Z"];
+  phone: Observable<string>|undefined;
+
+  constructor() {
+    this.showPhones();
+  }
+
+  showPhones() {
+    this.phone = interval(1000).pipe(map((i:number)=> this.phones[i%this.phones.length]));
+  }
 }
--- Laba6/Pipes2/src/app/app.module.ts
+++ Laba6/Pipes2/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormatPipe } from './format.pipe';
+import {FormsModule} from "@angular/forms";
+import { JoinPipe } from './join.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormatPipe,
+    JoinPipe
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
